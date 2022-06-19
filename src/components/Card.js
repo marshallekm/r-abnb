83224a3f34c4714a5398aecdeb37a70f6cfcc7bd
Erika Kawashima <marshallekm@gmail.com>
2022-06-19 15:32:20 +0900

    card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import CardPicture from '../images/katie-zaferes.png'
+import Star from '../images/star.png'
 
 export default function Card() {
   return (
-    <div>
-
-      <p>Info</p>
-      <p>Price</p>
-    </div>
+    <section className="card">
+      <img src={CardPicture} className= "card-pic" alt=""></img>
+      <p><img src ={Star} className="star"alt=""></img> 5.0 (6) USA</p>
+      <p>Life lessons with Katie Zaferes</p>
+      <p><nobr className="price">From $135</nobr>/person</p>
+    </section>
   )
 }
