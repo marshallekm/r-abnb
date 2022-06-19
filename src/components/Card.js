9e205be9d7b266928426c97b9240098ac20c6c32
Erika Kawashima <marshallekm@gmail.com>
2022-06-19 17:44:26 +0900

    card finished

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,9 +6,9 @@ export default function Card() {
   return (
     <section className="card">
       <img src={CardPicture} className= "card-pic" alt=""></img>
-      <p><img src ={Star} className="star"alt=""></img> 5.0 (6) USA</p>
+      <p><img src ={Star} className="star"alt=""></img> <span className="grey">5.0 (6) USA </span></p>
       <p>Life lessons with Katie Zaferes</p>
-      <p><nobr className="price">From $135</nobr>/person</p>
+      <p><nobr className="price">From $135</nobr><span className="grey">/person</span></p>
     </section>
   )
 }
