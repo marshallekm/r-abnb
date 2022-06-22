780270df536b5f3815f0a407ced9798229a62692
Erika Kawashima <marshallekm@gmail.com>
2022-06-22 20:44:40 +0900

    props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react'
 import Navbar from './components/Navbar.js'
 import Hero from './components/Hero.js'
 import Card from './components/Card.js'
+import myImage from "./images/katie-zaferes.png"
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+      img= {myImage}
+      rating ="5.0"
+      reviewCount = {6}
+      country ="USA"
+      title ="Life lessons with Katie Zaferes"
+      price = {136}
+      />
     </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import CardPicture from '../images/katie-zaferes.png'
 import Star from '../images/star.png'
+import "../images/katie-zaferes.png"
 
-export default function Card() {
+export default function Card(props) {
   return (
     <section className="card">
-      <img src={CardPicture} className= "card-pic" alt=""></img>
-      <p><img src ={Star} className="star"alt=""></img> <span className="grey">5.0 (6) USA </span></p>
-      <p>Life lessons with Katie Zaferes</p>
-      <p><nobr className="price">From $135</nobr><span className="grey">/person</span></p>
+      <img src={props.img} className= "card-pic" alt=""></img>
+      <p><img src ={Star} className="star"alt=""></img> <span className="grey">{props.rating}&nbsp;({props.reviewCount}) &nbsp;{props.country}</span></p>
+      <p>{props.title}</p>
+      <p><nobr className="price">From {props.price}</nobr><span className="grey">/person</span></p>
     </section>
   )
 }
