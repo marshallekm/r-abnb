e87dbf4afae2c172176941d6ef261a19f828436a
Erika Kawashima <marshallekm@gmail.com>
2022-06-26 11:04:38 +0900

    .

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,7 +14,7 @@ function badgeText() {
     <section className="card">
      {badgeText && <div className="sold-out">{badgeText()}</div>}
       <img src={props.item.coverImg} className= "card-pic" alt=""></img>
-      <p><img src = {imgStar} className="star"alt=""></img> <span className="grey">{props.item.stats.rating}&nbsp;({props.item.stats.reviewCount}) &nbsp;{props.location}</span></p>
+      <p><img src = {imgStar} className="star"alt=""></img> <span className="grey">{props.item.stats.rating}&nbsp;({props.item.stats.reviewCount}) &nbsp;{props.item.location}</span></p>
       <p>{props.item.title}</p>
       <p><nobr className="price">From {props.item.price}</nobr><span className="grey">/person</span></p>
     </section>
